0dc44d23c3ba26ef98d676fe5519f2388f5a3e22
bmscestudentcompanion <142423102+bmscestudentcompanion@users.noreply.github.com>
2023-09-01 11:07:13 +0530

    Update save_data.php

--- save_data.php
+++ save_data.php
@@ -9,8 +9,9 @@ $db = mysqli_init();
 mysqli_real_connect($db, $azure_mysql_host, $azure_mysql_username, $azure_mysql_password, $azure_mysql_dbname, $azure_mysql_port, NULL, MYSQLI_CLIENT_SSL);
 
 if (!$db) {
-    die("connection failed: " . mysqli_dbect_error());
+    die("Connection failed: " . mysqli_connect_error());
 }
+
 $create_table_query = "
     CREATE TABLE IF NOT EXISTS profile (
         id INT AUTO_INCREMENT PRIMARY KEY,
@@ -21,23 +22,33 @@ $create_table_query = "
         address VARCHAR(255) NOT NULL,
         father VARCHAR(255) NOT NULL,
         mother VARCHAR(255) NOT NULL,
-        mobileNumber INT(10) NOT NULL,
+        mobileNumber INT(10) NOT NULL
     )
 ";
-$firstName = $_POST['firstName'];
-$lastName = $_POST['lastName'];
-$gender = $_POST['gender'];
-$nationality = $_POST['nationality'];
-$address = $_POST['address'];
-$father = $_POST['father'];
-$mother = $_POST['mother'];
-$mobileNumber = $_POST['mobileNumber'];
-$insertSql = "INSERT INTO profile VALUES ('$firstName', '$lastName', '$gender', '$nationality', '$address', '$father', '$mother', '$mobileNumber')";
+
+if ($db->query($create_table_query) === TRUE) {
+    // Table creation successful
+} else {
+    echo "Error creating table: " . $db->error;
+}
+
+$firstName = mysqli_real_escape_string($db, $_POST['firstName']);
+$lastName = mysqli_real_escape_string($db, $_POST['lastName']);
+$gender = mysqli_real_escape_string($db, $_POST['gender']);
+$nationality = mysqli_real_escape_string($db, $_POST['nationality']);
+$address = mysqli_real_escape_string($db, $_POST['address']);
+$father = mysqli_real_escape_string($db, $_POST['father']);
+$mother = mysqli_real_escape_string($db, $_POST['mother']);
+$mobileNumber = mysqli_real_escape_string($db, $_POST['mobileNumber']);
+
+$insertSql = "INSERT INTO profile (firstName, lastName, gender, nationality, address, father, mother, mobileNumber) VALUES ('$firstName', '$lastName', '$gender', '$nationality', '$address', '$father', '$mother', '$mobileNumber')";
+
 if ($db->query($insertSql) === TRUE) {
     header("Location: dashboard.php");
-  echo "Profile saved successfully";
+    echo "Profile saved successfully";
 } else {
-  echo "Error: " . $insertSql . "<br>" . $db->error;
+    echo "Error: " . $insertSql . "<br>" . $db->error;
 }
+
 $db->close();
 ?>
