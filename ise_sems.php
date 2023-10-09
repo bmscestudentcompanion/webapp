2919212c6b443fbbdde5c9480ad7371d50fa54b9
bmscestudentcompanion <142423102+bmscestudentcompanion@users.noreply.github.com>
2023-10-09 23:49:39 +0530

    Update ise_sems.php

--- ise_sems.php
+++ ise_sems.php
@@ -103,21 +103,21 @@ body {
     <BR>
     <div style="align-content:center;">
       <ul style="font-family: 'Times New Roman', Times, serif; text-align: left">
-        <a href="cse_sems.php"><button class="glow-on-hover">First Semester</button></a>
+        <a href="ISE/isesem1.php"><button class="glow-on-hover">First Semester</button></a>
         <BR></br>
-        <a href="ece_sems.php"><button class="glow-on-hover">Second Semester</button></a>
+        <a href="ISE/isesem2.php"><button class="glow-on-hover">Second Semester</button></a>
         <BR></br>
-        <a href="mech_sems.php"><button class="glow-on-hover">Third Semester</button></a>
+        <a href="ISE/isesem3.php"><button class="glow-on-hover">Third Semester</button></a>
         <BR></br>
-        <a href="civil_sems.php"><button class="glow-on-hover">Fourth Semester</button></a>
+        <a href="ISE/isesem4.php"><button class="glow-on-hover">Fourth Semester</button></a>
         <BR></br>
-        <a href="chem_sems.php"><button class="glow-on-hover">Fifth Semester</button></a>
+        <a href="ISE/isesem5.php"><button class="glow-on-hover">Fifth Semester</button></a>
         <BR></br>
-        <a href="ise_sems.php"><button class="glow-on-hover">Sixth Semester</button></a>
+        <a href="ISE/isesem6.php"><button class="glow-on-hover">Sixth Semester</button></a>
         <BR></br>
-        <a href="mech_sems.php"><button class="glow-on-hover">Seventh Semester</button></a>
+        <a href="ISE/isesem7.php"><button class="glow-on-hover">Seventh Semester</button></a>
         <BR></br>
-        <a href="mech_sems.php"><button class="glow-on-hover">Eighth Semester</button></a>
+        <a href="ISE/isesem8.php"><button class="glow-on-hover">Eighth Semester</button></a>
         <BR></br>
       </ul>
     </div>
